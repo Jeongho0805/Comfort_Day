8c303e4a7005a9842b9a46877aa22acba7fe7d8a
JeonghoPark <pjhpjh0805@naver.com>
2023-06-09 19:40:20 +0900

    commit

--- src/main/java/com/jeongho/portfolio/controller/page/BoardController.java
+++ src/main/java/com/jeongho/portfolio/controller/page/BoardController.java
@@ -16,7 +16,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
