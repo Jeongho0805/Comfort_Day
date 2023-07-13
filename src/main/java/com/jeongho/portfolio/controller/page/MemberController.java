7722e0406348674e9feb13058294c6f6f4c4fb80
JeonghoPark <pjhpjh0805@naver.com>
2023-07-13 19:53:52 +0900

    commit

--- src/main/java/com/jeongho/portfolio/controller/page/MemberController.java
+++ src/main/java/com/jeongho/portfolio/controller/page/MemberController.java
@@ -25,6 +25,8 @@ public class MemberController {
 
     final private MemberService memberService;
 
+
+
     @GetMapping("/register")
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
